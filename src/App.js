30672b7b0b934efea53c5f7be4b1db62d01c5ea5
Shitanshu <shitanshukumar639@gmail.com>
2024-07-06 15:55:19 +0530

    updated the logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { useState } from "react";
+import Die from "./Die";
+
 function App() {
-  return <h1>hi chat</h1>;
+  const [dice, setDice] = useState(allNewDice());
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      let obj = {};
+      obj.value = Math.ceil(Math.random() * 6);
+      obj.isHeld = false;
+      newDice.push(obj);
+    }
+    return newDice;
+  }
+
+  function rollDice() {
+    setDice(allNewDice());
+  }
+
+  const diceElements = dice.map((die) => <Die value={die.value} />);
+
+  return (
+    <main>
+      <div className="dice-container">{diceElements}</div>
+      <button onClick={rollDice}>Roll</button>
+    </main>
+  );
 }
 
 export default App;
